afa433a9919c4689958db889e63f5f22f9e3c0b6
BahaI322 <zed_zed228@ro.ru>
2022-12-13 21:49:05 +0500

    fix img card and fix htaccess

--- carousel/mouse.js
+++ carousel/mouse.js
@@ -16,21 +16,21 @@ photo .addEventListener ('mousedown', (event) => {
 	const photo = document .getElementById ('photo');
 	const photo_cell = document .getElementById ('photo_cell');
 	photo .addEventListener('click', () => {
-	photo_cell.classList.toggle('move');
+		photo_cell.classList.toggle('move');
 	});
 });
 photo_cell.addEventListener('mousedown', (e) => {
 	x = e.offsetX;
 	y = e.offsetY;
 	move = true;
-  });
+});
   photo_cell.addEventListener('mousemove', (e) => {
 	if (move) {
 	  drawLine(context, x, y, e.offsetX, e.offsetY);
 	  x = e.offsetX;
 	  y = e.offsetY;
 	}
-  });
+});
   window.addEventListener('mouseup', (e) => {
 	if (move) {
 	  drawLine(context, x, y, e.offsetX, e.offsetY);
--- carousel/next _back.js
+++ carousel/next _back.js
@@ -1,20 +1,20 @@
-let photo = document.getElementById('.photo');
+let photo = document.getElementById('photo');
 let cellCount = 9;
 let selectedIndex = 0;
 
-const phot = document .getElementById ('photo');
+const photo = document .getElementById ('photo');
 photo .addEventListener ('photo', (event) => {
   let angle = selectedIndex / cell * -360;
   photo.style.transform = 'translateZ(-288px) rotateY(' + angle + 'deg)';
 })
 
-let backButton = document.getElementById('.back_button');
+let backButton = document.getElementById('back_button');
 backButton.addEventListener( 'click', (event) => {
   selectedIndex--;
   backButton.classList.rotatePhoto();
 });
 
-let nextButton = document.getElementById('.next_button');
+let nextButton = document.getElementById('next_button');
 nextButton.addEventListener( 'click',(event) => {
   selectedIndex++;
   nextButton.classList.rotatePhoto();
